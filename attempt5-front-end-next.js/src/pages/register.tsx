9e24d12a4a09bc6a91ec152ff3f39a8cd7bf46bd
Tristan <tantris.kantner@gmail.com>
2021-06-24 09:12:28 +0200

    Initialize git

---
+++ attempt5-front-end-next.js/src/pages/register.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+interface registerProps {}
+
+const Register: React.FC<registerProps> = ({}) => {
+  return (
+    <>
+      <div>Register</div>;
+    </>
+  );
+};
+export default Register;
